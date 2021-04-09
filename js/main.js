00ab0dc12118ba134d3b6c28367f376465dbd25d
MorCat <47464940+cmv007@users.noreply.github.com>
2021-04-09 14:42:54 +0300

    Responisve YouTube

--- js/main.js
+++ js/main.js
@@ -1,19 +1,47 @@
 const swiper = new Swiper('.channel-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
-
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+     1900: {
+       slidesPerView: 6
+     },
+     1600: {
+       slidesPerView: 5
+     },
+   
+     1300: {
+       slidesPerView: 4
+     },
+     1124: {
+       slidesPerView: 3
+     },
+     800: {
+       slidesPerView: 2
+     },
+  },
   // Navigation arrows
   navigation: {
-    nextEl: '.channel-button-next',
-    prevEl: '.channel-button-prev',
-  },
-})
+    nextEl: '#my-channel-1-prev',
+    prevEl: '#my-channel-1-next',
+  }
+});
 
 const swipers = new Swiper('.rec-channel-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 3,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1600:{
+      slidesPerView: 3
+    },
+    1100:{
+      slidesPerView: 2
+    },
+    
+  },
 
   // Navigation arrows
   navigation: {
@@ -25,11 +53,40 @@ const swipers = new Swiper('.rec-channel-slider', {
 const swiperfood = new Swiper('.food-channel-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
-
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+     1900: {
+       slidesPerView: 6
+     },
+     1600: {
+       slidesPerView: 5
+     },
+   
+     1300: {
+       slidesPerView: 4
+     },
+     1124: {
+       slidesPerView: 3
+     },
+     800: {
+       slidesPerView: 2
+     },
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.food-channel-button-next',
     prevEl: '.food-channel-button-prev',
   },
 });
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open');
+});
+
+if (document.documentElement.scrollWidth <= 640) {
+  this.channel-slider.destroy();
+  rec-channel-slider.destroy();
+  food-channel-slider.destroy();
+}
